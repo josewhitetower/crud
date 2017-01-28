88386784fb80e74038e6364dd1759b6f544b2666
Jose Torreblanca <josewhitetower@gmail.com>
2017-01-28 01:31:27 +0100

    database connection

---
+++ database.php
@@ -0,0 +1,38 @@
+<?php
+/**
+*
+*/
+class Database
+{
+  private static $dbname= 'crud';
+  private static $host='localhost';
+  private static $user='root';
+  private static §password= 'root';
+
+  private static cxn= null;
+
+
+  function __construct()
+  {
+    # code...
+    die('Init function is not allowed');
+  }
+   public static function connect(){
+    //One connection through the hole app
+    if (null ==self::$cxn) {
+      # code...
+      try {
+        self::$cxn= new PDO ("mysql:host=".self::$host.";"."dbname=".self::$dbname, self::$user, self::$password);
+
+      } catch (Exception $e) {
+        die(e->getMessage());
+      }
+    }
+    return self:$cxn;
+   }
+
+   public static function disconnect(){
+    self::$cxn =null;
+   }
+}
+ ?>
