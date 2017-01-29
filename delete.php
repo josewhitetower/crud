88d1f3b7a5cd8d76d1a33c7b27c9ef285f16d6b9
Jose Torreblanca <josewhitetower@gmail.com>
2017-01-29 21:30:37 +0100

    btn NO default Bootstrap

--- delete.php
+++ delete.php
@@ -39,7 +39,7 @@
                       <p class="alert alert-danger">Are you sure to delete ?</p>
                       <div class="form-actions">
                           <button type="submit" class="btn btn-danger">Yes</button>
-                          <a class="btn" href="index.php">No</a>
+                          <a class="btn btn-default" href="index.php">No</a>
                         </div>
                     </form>
                 </div>
